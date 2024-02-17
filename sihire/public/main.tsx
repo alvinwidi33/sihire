ce62526c05e901b05be2504914d73bdd76e60049
Alvin widinugroho <alvinwidinugroho@gmail.com>
2024-02-18 00:02:46 +0700

    coba lagi

---
+++ sihire/public/main.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom'; 
+
+ReactDOM.render(<React.StrictMode><Router /></React.StrictMode>, document.getElementById('root'));
