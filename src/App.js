e2d57e4ac3ac465dac0300214e28dce93ea889c9
Alvin Widi Nugroho <alvin.widi@ui.ac.id>
2024-03-06 15:13:25 +0700

    Apin

--- src/App.js
+++ src/App.js
@@ -8,15 +8,16 @@ import Services from './pages/services';
 import ContactUs from './pages/contact-us';
 import OurProjects from './pages/our-projects';
 import Careers from './pages/careers';
-// BARU NIH
 import AddJobApplication from './pages/add-job-application';
 import JobApplicationDetail from './pages/job-application-detail';
+import JobPosting from './pages/job-posting';
+import Applicants from './pages/applicants';
 
 function App() {
 
   return (
     <Router>
-      <Navbar />
+      {/* <Navbar /> */}
       <Routes>
         <Route path="/" exact element={<Home />} />
         <Route path="/our-team" element={<OurTeam />} />
@@ -26,6 +27,8 @@ function App() {
         <Route path="/careers" element={<Careers />} />
         <Route path='/add-job-application' element={<AddJobApplication />} />
         <Route path='/job-application-detail' element={<JobApplicationDetail />} />
+        <Route path='/job-posting-ga' element={<JobPosting />} />
+        <Route path='/applicants' element={<Applicants />} />
       </Routes>
     </Router>
   );
---
+++ src/components/sidebar.js
@@ -0,0 +1,105 @@
+import React, { useState } from 'react';
+import Logo from "../images/logo.png";
+import styled from 'styled-components';
+
+
+function Sidebar() {
+const [activePage, setActivePage] = useState('Job Posting');
+
+  const handleButtonClick = (page) => {
+    setActivePage(page);
+  };
+  return (
+    <React.Fragment>
+      <div className="vertical-line" style={{ marginLeft: '18%', height: '100%', borderLeft: '2px solid #2A3E4B', position: 'absolute', top: '0', bottom: '0' }}></div>
+      <div style={{ position: 'relative' }}>
+        <img src={Logo} alt="Logo" style={{ width: '169px', height: '48px', marginBottom: '20px', marginTop: '12px', marginLeft: '2%' }} />
+        <div className="horizontal-line" style={{ marginLeft:'18%', width: '82%', height: '2px', borderTop: '2px solid #2A3E4B', marginBottom: '20px', marginRight: '10px' }}></div>
+      </div>
+      <div className="sidebar" style={{ marginTop: '2px'}}>
+      <ul>
+        <li>
+          <button
+            style={{
+              fontFamily: 'Inter, sans-serif',
+              fontWeight: 'medium',
+              display: 'block',
+              marginBottom: '16px',
+              border: 'none',
+              background: activePage === 'Job Posting' ? '#E2E7F0' : '#fff',
+              height:'32px',
+              width: '18%',
+              textAlign: 'left',
+              paddingLeft: '2%', 
+            }}
+            onClick={() => handleButtonClick('Job Posting')}
+          >
+            Job Posting
+          </button>
+        </li>
+        <li>
+          <button
+            style={{
+              fontFamily: 'Inter, sans-serif',
+              fontWeight: 'medium',
+              display: 'block',
+              marginBottom: '16px',
+              border: 'none',
+              background: activePage === 'Applicants' ? '#E2E7F0' : '#fff',
+              height:'32px',
+              width: '18%', 
+              textAlign: 'left',
+              paddingLeft: '2%',  
+            }}
+            onClick={() => handleButtonClick('Applicants')}
+          >
+            Applicants
+          </button>
+        </li>
+        <li>
+          <button
+            style={{
+              fontFamily: 'Inter, sans-serif',
+              fontWeight: 'medium',
+              display: 'block',
+              marginBottom: '16px',
+              border: 'none',
+              background: activePage === 'Feedback' ? '#E2E7F0' : '#fff',
+              height:'32px',
+              width: '18%',
+              textAlign: 'left',
+              paddingLeft: '2%', 
+            }}
+            onClick={() => handleButtonClick('Feedback')}
+          >
+            Feedback
+          </button>
+        </li>
+        <li>
+          <button
+  style={{
+    fontFamily: 'Inter, sans-serif',
+    fontWeight: 'medium',
+    display: 'block',
+    marginBottom: '16px',
+    border: 'none',
+    background: activePage === 'On Boarding' ? '#E2E7F0' : '#fff',
+    height:'32px',
+    width: '18%',
+    textAlign: 'left',
+    paddingLeft: '2%', 
+  }}
+  onClick={() => handleButtonClick('On Boarding')}
+>
+  On Boarding
+</button>
+
+        </li>
+      </ul>
+    </div>
+
+    </React.Fragment>
+  );
+}
+
+export default Sidebar;
