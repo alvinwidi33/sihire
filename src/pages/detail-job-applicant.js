828ff0d32049df50da4545ced4463887e36c96d5
Alvin Widi Nugroho <alvin.widi@ui.ac.id>
2024-03-10 13:11:58 +0700

    Apin

--- src/pages/detail-job-applicant.js
+++ src/pages/detail-job-applicant.js
@@ -25,40 +25,52 @@ function formatDateTime(datetimeString) {
   }, [id]);
 
   return (
-    <React.Fragment>
-      <p style={{ marginLeft: "22%", fontWeight: "bold", fontSize: "32px", color: "#2A3E4B", position: "absolute" }}>Job</p>
-      <Sidebar />
-      <div className="detail-job" style={{ position: "relative" }}>
-        {job && (
-  <React.Fragment key={job.id}>
-    <p style={{ marginLeft: "22%", fontWeight: "bold", fontSize: "32px", color: "#2A3E4B", marginTop: "-190px", marginBottom: "12px" }}>
-      {job.job_name}
-    </p>
-    <p style={{ marginLeft: "22%", fontWeight: "bold", fontSize: "24px", color: "#2A3E4B", marginTop: "-0px", marginBottom: "4px" }}>
-      Job Description
-    </p>
-    <p style={{ marginLeft: "22%", fontSize: "16px", color: "#2A3E4B", marginBottom: "12px" }}>
-        {job.description}
-    </p>
-    <p style={{ marginTop:"20%",marginLeft: "22%", fontWeight: "bold", fontSize: "24px", color: "#2A3E4B", marginBottom: "4px" }}>
-        Closed Date
-    </p>
-    <p style={{ marginLeft: "22%", fontSize: "16px", color: "#2A3E4B", marginBottom: "12px" }}>
-  {job.datetime_closes && formatDateTime(job.datetime_closes)}
-</p>
-<Link to={`/add-job-application/${job.id}`}>
-<button style={{width:"420px", padding: "8px", fontSize: "16px", fontFamily: 'Inter, sans-serif', fontWeight: 'bold', color: "#fff", background: "#2A3E4B", 
-      borderRadius: "6px", cursor: "pointer", marginTop: "4%", marginBottom: "12px", border: "2px solid #2A3E4B",
-      marginLeft: "44%", position:"absolute"}}>
-        Apply
-    </button>
-</Link>
-  </React.Fragment>
-)}
-
-      </div>
+  <React.Fragment>
+    <div style={{ marginLeft: '22%', position: 'absolute', marginBottom: '40px', marginTop:'80px' }}>
+  <Link to='/job-list-applicant' style={{ textDecoration: 'none', color: '#2A3E4B', cursor: 'pointer' }}>
+    <p style={{ display: 'inline', marginLeft: '4px' }}>List Job</p>
+  </Link>
+  <span style={{ display: 'inline', marginLeft: '10px' }}>{'>'}</span>
+  {job && (
+    <React.Fragment key={job.id}>
+      <Link to={`/job-list-applicant/${job.id}`} style={{ textDecoration: 'none', color: '#2A3E4B', cursor: 'pointer' }}>
+        <p style={{ display: 'inline', marginLeft: '10px' }}>Job Details</p>
+      </Link>
     </React.Fragment>
-  );
+  )}
+</div>
+
+    <p style={{ marginLeft: "22%", fontWeight: "bold", fontSize: "32px", color: "#2A3E4B", position: "absolute", }}>Job</p>
+    <Sidebar />
+    <div className="detail-job" style={{ position: "relative" }}>
+      {job && (
+        <React.Fragment key={job.id}>
+          <p style={{ marginLeft: "22%", fontWeight: "bold", fontSize: "32px", color: "#2A3E4B", marginTop: "-170px", marginBottom: "12px" }}>
+            {job.job_name}
+          </p>
+          <p style={{ marginLeft: "22%", fontWeight: "bold", fontSize: "24px", color: "#2A3E4B", marginTop: "-0px", marginBottom: "4px" }}>
+            Job Description
+          </p>
+          <p style={{ marginLeft: "22%", fontSize: "16px", color: "#2A3E4B", marginBottom: "12px" }}>
+            {job.description}
+          </p>
+          <p style={{ marginTop: "20%", marginLeft: "22%", fontWeight: "bold", fontSize: "24px", color: "#2A3E4B", marginBottom: "4px" }}>
+            Closed Date
+          </p>
+          <p style={{ marginLeft: "22%", fontSize: "16px", color: "#2A3E4B", marginBottom: "12px" }}>
+            {job.datetime_closes && formatDateTime(job.datetime_closes)}
+          </p>
+          <Link to={`/add-job-application/${job.id}`}>
+            <button style={{ width: "420px", padding: "8px", fontSize: "16px", fontFamily: 'Inter, sans-serif', fontWeight: 'bold', color: "#fff", background: "#2A3E4B", borderRadius: "6px", cursor: "pointer", marginTop: "4%", marginBottom: "12px", border: "2px solid #2A3E4B", marginLeft: "44%", position: "absolute" }}>
+              Apply
+            </button>
+          </Link>
+        </React.Fragment>
+      )}
+    </div>
+  </React.Fragment>
+);
+
 }
 
 export default DetailJobApplicant;
