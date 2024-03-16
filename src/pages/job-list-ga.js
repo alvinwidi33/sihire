a44c4eac54a85df02423d87fe55f3965056d721e
Alvin Widi Nugroho <alvin.widi@ui.ac.id>
2024-03-16 22:31:40 +0700

    Apin

--- src/pages/job-list-ga.js
+++ src/pages/job-list-ga.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Sidebar from '../components/sidebar';
+import Sidebar from '../components/sidebar'; // Make sure to import the Sidebar component properly
 import { Link, useNavigate } from 'react-router-dom';
 
 function JobListGA() {
@@ -8,11 +8,10 @@ function JobListGA() {
   const [successMessage, setSuccessMessage] = useState('');
 
   function formatDateTime(datetimeString) {
-  const options = { day: 'numeric', month: 'long', year: 'numeric' };
-  const formattedDate = new Date(datetimeString).toLocaleDateString('id-ID', options);
-  return formattedDate;
-}
-
+    const options = { day: 'numeric', month: 'long', year: 'numeric' };
+    const formattedDate = new Date(datetimeString).toLocaleDateString('id-ID', options);
+    return formattedDate;
+  }
 
   useEffect(() => {
     const getJobs = async () => {
@@ -31,74 +30,74 @@ function JobListGA() {
     getJobs();
   }, []);
 
-const handleClose = async (id) => {
-  const currentDate = new Date();
-const formattedDate = currentDate.toISOString();
+  const handleClose = async (id) => {
+    const currentDate = new Date();
+    const formattedDate = currentDate.toISOString();
 
-  const confirmClose = window.confirm('Apakah Anda yakin ingin menutup pekerjaan ini?');
-  if (!confirmClose) return;
+    const confirmClose = window.confirm('Apakah Anda yakin ingin menutup pekerjaan ini?');
+    if (!confirmClose) return;
 
-  try {
-    const response = await fetch(`https://sihire-be.vercel.app/api/job-posting/edit/${id}/`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ datetime_closes: formattedDate }),
-    });
-    if (response.ok) {
-      setJobs(prevJobs => prevJobs.map(job => {
-        if (job.id === id) {
-          return { ...job, datetime_closes: formattedDate };
-        }
-        return job;
-      }));
-      setSuccessMessage("Pekerjaan berhasil ditutup!");
-      setTimeout(() => {
-        setSuccessMessage('');
-        navigate("/job-list-ga");
-      }, 5000);
-    } else {
-      console.error('Failed to update closing date');
+    try {
+      const response = await fetch(`https://sihire-be.vercel.app/api/job-posting/edit/${id}/`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ datetime_closes: formattedDate }),
+      });
+      if (response.ok) {
+        setJobs(prevJobs => prevJobs.map(job => {
+          if (job.id === id) {
+            return { ...job, datetime_closes: formattedDate };
+          }
+          return job;
+        }));
+        setSuccessMessage("Pekerjaan berhasil ditutup!");
+        setTimeout(() => {
+          setSuccessMessage('');
+          navigate("/job-list-ga");
+        }, 5000);
+      } else {
+        console.error('Failed to update closing date');
+      }
+    } catch (error) {
+      console.error('Error updating closing date:', error);
     }
-  } catch (error) {
-    console.error('Error updating closing date:', error);
-  }
-};
-
+  };
 
   return (
     <React.Fragment>
       <p style={{ marginLeft: "22%", fontWeight: "bold", fontSize: "32px", color: "#2A3E4B", position: "absolute" }}>Job</p>
-      <Sidebar />
+      <Sidebar /> 
       <div className="job-posting" style={{ position: "relative" }}>
-        <p style={{ marginLeft: "22%", fontWeight: "bold", fontSize: "32px", color: "#2A3E4B", marginTop: "-190px", marginBottom: "12px" }}>Jobs Available</p>
+        <p style={{ marginLeft: "22%", fontWeight: "bold", fontSize: "32px", color: "#2A3E4B", marginTop: "-190px", marginBottom: "12px" }}>Lowongan Pekerjaan</p>
         <Link to="/add-job-posting">
           <button style={{ width: "180px", padding: "8px", fontSize: "16px", fontFamily: 'Inter, sans-serif', fontWeight: 'bold', color: "#fff", background: "#2A3E4B",
             borderRadius: "6px", cursor: "pointer",
-            marginTop: "-52px", marginBottom: "12px", marginLeft: "88%", position: "absolute", border: "2px solid #2A3E4B", }}>
+            marginTop: "-52px", marginBottom: "12px", marginLeft: "80%", position: "absolute", border: "2px solid #2A3E4B", }}>
             Tambah Pekerjaan
           </button>
         </Link>
         {jobs && (
-          <table style={{ marginLeft: "22%", borderCollapse: "collapse", width: "80%" }}>
+          <table style={{ marginLeft: "22%", borderCollapse: "collapse", width: "70%" }}>
             <thead>
               <tr>
-                <th style={{ border: "2px solid #2A3E4B", padding: "8px", textAlign: "left", fontWeight: "bold" }}>Job Name</th>
-                <th style={{ border: "2px solid #2A3E4B", padding: "8px", textAlign: "left", fontWeight: "bold" }}>Close Date</th>
-                <th style={{ border: "2px solid #2A3E4B", padding: "8px", textAlign: "left", fontWeight: "bold" }}>Tutup</th>
-                <th style={{ border: "2px solid #2A3E4B", padding: "8px", textAlign: "left", fontWeight: "bold" }}>Detail</th>
+                <th style={{ border: "2px solid #2A3E4B", padding: "4px", textAlign: "center", fontWeight: "bold" }}>JPekerjaan</th>
+                <th style={{ border: "2px solid #2A3E4B", padding: "4px", textAlign: "center", fontWeight: "bold" }}>Tanggal Tutup</th>
+                <th style={{ border: "2px solid #2A3E4B", padding: "4px", textAlign: "center", fontWeight: "bold" }}>Tutup</th>
+                <th style={{ border: "2px solid #2A3E4B", padding: "4px", textAlign: "center", fontWeight: "bold" }}>Detail</th>
               </tr>
             </thead>
             <tbody>
+            
               {jobs.map(job => (
-                <tr key={job.id}>
+                <tr key={job.id} style={job.datetime_closes && new Date(job.datetime_closes) < new Date() ? { backgroundColor: "#D2D2D2" } : {}}>
                   <td style={{ border: "2px solid #2A3E4B", padding: "8px" }}>{job.job_name}</td>
                   <td style={{ border: "2px solid #2A3E4B", padding: "8px" }}>{job.datetime_closes && formatDateTime(job.datetime_closes)}</td>
-                  <td style={{ border: "2px solid #2A3E4B", padding: "8px" }}>
+                  <td style={{ border: "2px solid #2A3E4B", padding: "8px", textAlign:"center"  }}>
                     <button onClick={() => handleClose(job.id)} style={{ padding: "8px", fontSize: "16px", fontFamily: 'Inter, sans-serif', fontWeight: 'bold', color: "#fff", borderRadius: "6px", cursor: "pointer", background: "#2A3E4B", border: "none", marginRight: "5px" }}>Tutup</button>
                   </td>
-                  <td style={{ border: "2px solid #2A3E4B", padding: "8px" }}>
+                  <td style={{ border: "2px solid #2A3E4B", padding: "8px", textAlign:"center" }}>
                     <Link to={`/job-list-ga/${job.id}`}>
                       <button style={{ padding: "8px", fontSize: "16px", fontFamily: 'Inter, sans-serif', fontWeight: 'bold', color: "#2A3E4B", borderRadius: "6px", cursor: "pointer", border: "2px solid #2A3E4B" }}>Detail</button>
                     </Link>
@@ -126,7 +125,7 @@ const formattedDate = currentDate.toISOString();
           {successMessage}
         </p>
       )}
-    </React.Fragment>
+      </React.Fragment>
   );
 }
 
--- src/pages/job-list-other.js
+++ src/pages/job-list-other.js
@@ -10,6 +10,12 @@ function JobListInternal() {
   return formattedDate;
 }
 
+  function formatDateTime(datetimeString) {
+    const options = { day: 'numeric', month: 'long', year: 'numeric' };
+    const formattedDate = new Date(datetimeString).toLocaleDateString('id-ID', options);
+    return formattedDate;
+  }
+
   useEffect(() => {
     const getJobs = async () => {
       await fetch('https://sihire-be.vercel.app/api/job-posting/get-internal/')
@@ -21,27 +27,33 @@ function JobListInternal() {
     getJobs();
   }, []);
 
+  function isJobClosed(datetimeString) {
+    const closingDate = new Date(datetimeString);
+    const currentDate = new Date();
+    return closingDate < currentDate;
+  }
+
   return (
     <React.Fragment>
       <p style={{ marginLeft: "22%", fontWeight: "bold", fontSize: "32px", color: "#2A3E4B", position: "absolute" }}>Job</p>
       <Sidebar />
       <div className="job-posting" style={{ position: "relative" }}>
-        <p style={{ marginLeft: "22%", fontWeight: "bold", fontSize: "32px", color: "#2A3E4B", marginTop: "-190px", marginBottom:"12px" }}>Jobs Available</p>
+        <p style={{ marginLeft: "22%", fontWeight: "bold", fontSize: "32px", color: "#2A3E4B", marginTop: "-190px", marginBottom:"12px" }}>Lowongan</p>
         {jobs && (
-          <table style={{ marginLeft: "22%", borderCollapse: "collapse", width: "80%" }}>
-            <thead>
+          <table style={{ marginLeft: "22%", borderCollapse: "collapse", width: "70%", }}>
+            <thead style={{}}>
               <tr>
-                <th style={{ border: "2px solid #2A3E4B", padding: "8px", textAlign: "left", fontWeight: "bold" }}>Job Name</th>
-                <th style={{ border: "2px solid #2A3E4B", padding: "8px", textAlign: "left", fontWeight: "bold" }}>Datetime Close</th>
-                <th style={{ border: "2px solid #2A3E4B", padding: "8px", textAlign: "left", fontWeight: "bold" }}>Detail</th>
+                <th style={{ border: "2px solid #2A3E4B", padding: "8px", textAlign: "center", fontWeight: "bold",  }}>Pekerjaan</th>
+                <th style={{ border: "2px solid #2A3E4B", padding: "8px", textAlign: "center", fontWeight: "bold" }}>Tanggal Tutup</th>
+                <th style={{ border: "2px solid #2A3E4B", padding: "8px", textAlign: "center", fontWeight: "bold" }}>Detail</th>
               </tr>
             </thead>
             <tbody>
               {jobs.map(job => (
-                <tr key={job.id}>
+                <tr key={job.id} style={isJobClosed(job.datetime_closes) ? { backgroundColor: "#D2D2D2" } : {}}>
                   <td style={{ border: "2px solid #2A3E4B", padding: "8px", fontFamily: 'Inter, sans-serif', fontWeight: 'bold', fontSize: "24px", color: "#2A3E4B" }}>{job.job_name}</td>
-                  <td style={{ border: "2px solid #2A3E4B", padding: "8px" }}>{job.datetime_closes && formatDateTime(job.datetime_closes)}</td>
-                  <td style={{ border: "2px solid #2A3E4B", padding: "8px" }}>
+                  <td style={{ border: "2px solid #2A3E4B", padding: "8px" }}>{formatDateTime(job.datetime_closes)}</td>
+                  <td style={{ border: "2px solid #2A3E4B", padding: "8px", "textAlign":"center" }}>
                     <Link to={`/job-list-other/${job.id}`}>
                       <button style={{width:"90px",padding: "8px",fontSize: "16px",fontFamily: 'Inter, sans-serif',fontWeight: 'bold',color: "#2A3E4B",borderRadius: "6px",cursor: "pointer",border: "2px solid #2A3E4B",}}>
                         Detail
