78b9a3e45257d285ef48293960ff36db2d383966
Alicia Kirana Utomo <alicia.kirana@ui.ac.id>
2024-04-14 08:16:19 +0700

    Kira

--- src/pages/my-job-application.js
+++ src/pages/my-job-application.js
@@ -5,24 +5,52 @@ import Sidebar from "../components/sidebar-applicant";
 
 const MyJobApplication = () => {
   const { applicant } = useParams();
-  const [jobApplications, setJobApplications] = useState(null);
+  const [jobApplications, setJobApplications] = useState([]);
+  const [interviews, setInterviews] = useState([]);
+  const [onboardings, setOnboardings] = useState([]);
   const [activeTab, setActiveTab] = useState("applications");
 
+  const getJobApplications = async () => {
+    try {
+      const response = await fetch(
+        `https://sihire-be.vercel.app/api/job-application/get/${applicant}/`
+      );
+      const data = await response.json();
+      setJobApplications(data);
+    } catch (error) {
+      console.error("Error fetching job:", error);
+    }
+  };
+
+  const getInterviews = async () => {
+    try {
+      const response = await fetch(
+        `https://sihire-be.vercel.app/api/interview/get-list-interview/${applicant}/`
+      );
+      const data = await response.json();
+      setInterviews(data);
+    } catch (error) {
+      console.error("Error fetching interviews:", error);
+    }
+  };
+
+  const getOnboardings = async () => {
+    try {
+      const response = await fetch(
+        `https://sihire-be.vercel.app/api/onboarding/get-list-onboarding/${applicant}/`
+      );
+      const data = await response.json();
+      setOnboardings(data);
+    } catch (error) {
+      console.error("Error fetching interviews:", error);
+    }
+  };
+
   useEffect(() => {
-    const getJobApplications = async () => {
-      try {
-        const response = await fetch(
-          `https://sihire-be.vercel.app/api/job-application/get/${applicant}/`
-        );
-        const data = await response.json();
-        setJobApplications(data);
-        console.log("data", data);
-      } catch (error) {
-        console.error("Error fetching job:", error);
-      }
-    };
     getJobApplications();
-  }, [applicant]);
+    getInterviews();
+    getOnboardings();
+  }, []);
 
   const TabContainer = styled.div`
     display: flex;
@@ -107,7 +135,7 @@ const MyJobApplication = () => {
       <Sidebar />
 
       <div
-        style={{ marginLeft: "22%", position: "absolute", marginTop: "-80px" }}
+        style={{ marginLeft: "22%", position: "absolute", marginTop: "-180px" }}
         className="w-9/12"
       >
         <TabContainer>
@@ -134,7 +162,7 @@ const MyJobApplication = () => {
         <ContentContainer active={activeTab === "applications"}>
           <SubTitle>My Applications</SubTitle>
           <Table>
-            <thead>
+            <thead style={{ backgroundColor: "#D2D2D2" }}>
               <tr>
                 <Th>Pekerjaan</Th>
                 <Th>Status</Th>
@@ -142,7 +170,7 @@ const MyJobApplication = () => {
                 <Th>Withdraw</Th>
               </tr>
             </thead>
-            <tbody style={{ backgroundColor: "#D2D2D2" }}>
+            <tbody>
               {jobApplications &&
                 jobApplications.map((jobApplication) => (
                   <tr key={jobApplication.id}>
@@ -164,12 +192,70 @@ const MyJobApplication = () => {
 
         <ContentContainer active={activeTab === "interviews"}>
           <SubTitle>My Interviews</SubTitle>
-          {/* Content for My Interviews */}
+          <Table>
+            <thead style={{ backgroundColor: "#D2D2D2" }}>
+              <tr>
+                <Th>Pekerjaan</Th>
+                <Th>Tanggal</Th>
+                <Th>Waktu</Th>
+                <Th>Status</Th>
+                <Th>Action</Th>
+              </tr>
+            </thead>
+            <tbody>
+              {interviews &&
+                interviews.map((interview) => (
+                  <tr key={interview.id}>
+                    <Td>{interview.job_application_id.job.job_name}</Td>
+                    <Td>{interview.datetime_start.split("T")[0]}</Td>
+                    <Td>{interview.datetime_start.slice(11, 16)}</Td>
+                    <Td>{interview.confirm}</Td>
+                    {interview.confirm === "Confirm" && (
+                      <Button primary>Batalkan</Button>
+                    )}
+                    {interview.confirm === "Not Confirm" && (
+                      <Td>
+                        <Button primary>Konfirmasi</Button>
+                      </Td>
+                    )}
+                  </tr>
+                ))}
+            </tbody>
+          </Table>
         </ContentContainer>
 
         <ContentContainer active={activeTab === "onboarding"}>
           <SubTitle>My Onboarding</SubTitle>
-          {/* Content for My Onboarding */}
+          <Table>
+            <thead style={{ backgroundColor: "#D2D2D2" }}>
+              <tr>
+                <Th>Pekerjaan</Th>
+                <Th>Tanggal</Th>
+                <Th>Waktu</Th>
+                <Th>Status</Th>
+                <Th>Action</Th>
+              </tr>
+            </thead>
+            <tbody>
+              {onboardings &&
+                onboardings.map((onboarding) => (
+                  <tr key={onboarding.id}>
+                    <Td>{onboarding.job_application_id.job.job_name}</Td>
+                    <Td>{onboarding.datetime_start.split("T")[0]}</Td>
+                    <Td>{onboarding.datetime_start.slice(11, 16)}</Td>
+                    <Td>{onboarding.confirm}</Td>
+                    {onboarding.confirm === "Confirm" && (
+                      <Button primary>Batalkan</Button>
+                    )}
+                    {onboarding.confirm === "Not Confirm" && (
+                      <Td>
+                        <Button primary>Konfirmasi</Button>
+                      </Td>
+                    )}
+                  </tr>
+                ))}
+            </tbody>
+          </Table>
         </ContentContainer>
       </div>
     </React.Fragment>
