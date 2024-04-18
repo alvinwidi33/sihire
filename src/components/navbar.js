c29ae52348e15ef5ec66d5c19e5a8188601d6612
Alvin widinugroho <alvinwidinugroho@gmail.com>
2024-04-18 08:10:24 +0700

    company profile and navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -14,6 +14,8 @@ function Navbar() {
         padding: '10px',
         textAlign: 'right',
         display: 'flex',
+        justifyContent: 'space-between', 
+        alignItems: 'center', 
         position:"fixed",
         width:"100%",
         top:"0",
@@ -21,14 +23,12 @@ function Navbar() {
     };
 
     const linkStyle = {
-        marginLeft: '2.8%',
         fontFamily: 'Inter, sans-serif',
         fontSize: '16px',
         fontWeight: 'bold',
-        justifyContent: 'flex-end',
-        marginTop: '10px',
         textDecoration: 'none',
         position: 'relative',
+        marginRight: '20px', 
     };
 
     const activeLinkStyle = {
@@ -39,16 +39,16 @@ function Navbar() {
 
     return (
         <div className='navbar' style={navbarStyle}>
+            <img src={Logo} alt="Logo" style={{ width: '169px', height: '48px'}} />
             <div>
-                <img src={Logo} alt="Logo" style={{ width: '169px', height: '48px', marginRight: '400px' }} />
+                <Link to="/" style={activeLink === '/' ? { ...linkStyle, ...activeLinkStyle } : linkStyle} onClick={() => handleSetActiveLink('/')}>Home</Link>
+                <Link to="/our-team" style={activeLink === '/our-team' ? { ...linkStyle, ...activeLinkStyle } : linkStyle} onClick={() => handleSetActiveLink('/our-team')}>Our Team</Link>
+                <Link to="/services" style={activeLink === '/services' ? { ...linkStyle, ...activeLinkStyle } : linkStyle} onClick={() => handleSetActiveLink('/services')}>Services</Link>
+                <Link to="/our-projects" style={activeLink === '/our-projects' ? { ...linkStyle, ...activeLinkStyle } : linkStyle} onClick={() => handleSetActiveLink('/our-projects')}>Our Projects</Link>
+                <Link to="/contact-us" style={activeLink === '/contact-us' ? { ...linkStyle, ...activeLinkStyle } : linkStyle} onClick={() => handleSetActiveLink('/contact-us')}>Contact Us</Link>
+                <Link to="/careers" style={activeLink === '/careers' ? { ...linkStyle, ...activeLinkStyle } : linkStyle} onClick={() => handleSetActiveLink('/careers')}>Careers</Link>
+                <Link to="/dashb-job-appli" style={activeLink === '/dashb-job-appli' ? { ...linkStyle, ...activeLinkStyle } : linkStyle} onClick={() => handleSetActiveLink('/dashb-job-appli')}>Dashboard</Link>
             </div>
-            <Link to="/" style={activeLink === '/' ? { ...linkStyle, ...activeLinkStyle } : linkStyle} onClick={() => handleSetActiveLink('/')}>Home</Link>
-            <Link to="/our-team" style={activeLink === '/our-team' ? { ...linkStyle, ...activeLinkStyle } : linkStyle} onClick={() => handleSetActiveLink('/our-team')}>Our Team</Link>
-            <Link to="/services" style={activeLink === '/services' ? { ...linkStyle, ...activeLinkStyle } : linkStyle} onClick={() => handleSetActiveLink('/services')}>Services</Link>
-            <Link to="/our-projects" style={activeLink === '/our-projects' ? { ...linkStyle, ...activeLinkStyle } : linkStyle} onClick={() => handleSetActiveLink('/our-projects')}>Our Projects</Link>
-            <Link to="/contact-us" style={activeLink === '/contact-us' ? { ...linkStyle, ...activeLinkStyle } : linkStyle} onClick={() => handleSetActiveLink('/contact-us')}>Contact Us</Link>
-            <Link to="/careers" style={activeLink === '/careers' ? { ...linkStyle, ...activeLinkStyle } : linkStyle} onClick={() => handleSetActiveLink('/careers')}>Careers</Link>
-            <Link to="/dashb-job-appli" style={activeLink === '/dashb-job-appli' ? { ...linkStyle, ...activeLinkStyle } : linkStyle} onClick={() => handleSetActiveLink('/dashb-job-appli')}>Dashboard</Link>
             <style>{`
                 @keyframes moveUnderline {
                     from {
